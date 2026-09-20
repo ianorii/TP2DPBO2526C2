0d1f72bf676fa19f2c404c3260abb82afd24e88e
rian <rian22anugrah@gmail.com>
2026-09-20 20:22:12 +0700

    first commit

---
+++ CPP/Main.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+#define int long long
+#define float long double
+
+using namespace std;
+
+typedef vector<int> v;
+typedef vector<v> vv;
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    print("a");
+
+    return 0;
+}
