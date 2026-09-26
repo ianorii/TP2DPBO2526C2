726cd802abd4780a8445930a6a37e3e3e6dea806
rian <rian22anugrah@gmail.com>
2026-09-26 09:37:46 +0700

    feat : add 3 class

---
+++ CPP/Jaket.cpp
@@ -0,0 +1,34 @@
+// Class Jaket
+class Jaket{
+    private:
+        // Atribut
+        string jenis_penutup;
+        bool kupluk;
+        int jumlah_saku;
+
+    public:
+        // Empty constructor
+        Jaket() {}
+
+        // Constructor with parameter
+        Jaket(string jenis_penutup, bool kupluk, int jumlah_saku) {
+            this->jenis_penutup = jenis_penutup;
+            this->kupluk = kupluk;
+            this->jumlah_saku = jumlah_saku;
+        }
+
+        // Setter and getter for jenis penutup
+        void setJenisPenutup(string jenis_penutup) {this->jenis_penutup = jenis_penutup;}
+        stirng getJenisPenutup() {return jenis_penutup;}
+        
+        // Setter and getter for kupluk
+        void setKupluk(bool kupluk) {this->kupluk = kupluk;}
+        stirng getJenisKupluk() {return kupluk;}
+        
+        // Setter and getter for jumlah saku
+        void setJumlahSaku(int jumlah_saku) {this->jumlah_saku = jumlah_saku;}
+        stirng getJumlahSaku() {return jumlah_saku;}
+
+        // Destructor
+        ~Jaket() {}
+};
